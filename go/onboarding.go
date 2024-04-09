4e59a0ecdf128ec6b7bf8156a2aec95ddcd9a463
Amay Shah <amayvs@gmail.com>
2024-04-09 16:03:04 +0530

    [go] Replace caigo with gnark-crypto for signing (#62)

--- go/onboarding.go
+++ go/onboarding.go
@@ -5,11 +5,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"math/big"
 	"net/http"
 	"os"
 	"strconv"
 	"time"
 
+	starkcurve "github.com/consensys/gnark-crypto/ecc/stark-curve"
+	"github.com/consensys/gnark-crypto/ecc/stark-curve/ecdsa"
+	"github.com/consensys/gnark-crypto/ecc/stark-curve/fr"
 	"github.com/dontpanicdao/caigo"
 	"github.com/dontpanicdao/caigo/types"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -53,6 +57,34 @@ func GenerateParadexAccount(config SystemConfigResponse, ethPrivateKey string) (
 	return dexPrivateKey, dexPublicKey, dexAccountAddress
 }
 
+// Get ECDSA private key from string
+func GetEcdsaPrivateKey(pk string) *ecdsa.PrivateKey {
+	privateKey := types.StrToFelt(pk).Big()
+
+	// Generate public key
+	_, g := starkcurve.Generators()
+	ecdsaPublicKey := new(ecdsa.PublicKey)
+	ecdsaPublicKey.A.ScalarMultiplication(&g, privateKey)
+
+	// Generate private key
+	pkBytes := privateKey.FillBytes(make([]byte, fr.Bytes))
+	buf := append(ecdsaPublicKey.Bytes(), pkBytes...)
+	ecdsaPrivateKey := new(ecdsa.PrivateKey)
+	ecdsaPrivateKey.SetBytes(buf)
+	return ecdsaPrivateKey
+}
+
+func GnarkSign(messageHash *big.Int, privateKey string) (r, s *big.Int, err error) {
+	ecdsaPrivateKey := GetEcdsaPrivateKey(privateKey)
+	sigBin, err := ecdsaPrivateKey.Sign(messageHash.Bytes(), nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	r = new(big.Int).SetBytes(sigBin[:fr.Bytes])
+	s = new(big.Int).SetBytes(sigBin[fr.Bytes:])
+	return r, s, nil
+}
+
 // `POST /onboarding`
 func PerformOnboarding(
 	config SystemConfigResponse,
@@ -63,13 +95,12 @@ func PerformOnboarding(
 
 ) {
 	dexAccountAddressBN := types.HexToBN(dexAccountAddress)
-	dexPrivateKeyBN := types.HexToBN(dexPrivateKey)
 
 	// Get message hash and signature
 	message := &OnboardingPayload{Action: "Onboarding"}
 	typedData, _ := NewVerificationTypedData(VerificationTypeOnboarding, config.ChainId)
 	messageHash, _ := typedData.GetMessageHash(dexAccountAddressBN, message, caigo.StarkCurve{})
-	r, s, _ := caigo.Curve.Sign(messageHash, dexPrivateKeyBN)
+	r, s, _ := GnarkSign(messageHash, dexPrivateKey)
 
 	// URL
 	onboardingUrl := fmt.Sprintf("%s/onboarding", PARADEX_HTTP_URL)
@@ -99,7 +130,6 @@ func GetJwtToken(
 	dexPrivateKey string,
 ) string {
 	dexAccountAddressBN := types.HexToBN(dexAccountAddress)
-	dexPrivateKeyBN := types.HexToBN(dexPrivateKey)
 
 	// Get timestamp and expiration
 	now := time.Now().Unix()
@@ -116,7 +146,7 @@ func GetJwtToken(
 	}
 	typedData, _ := NewVerificationTypedData(VerificationTypeAuth, config.ChainId)
 	messageHash, _ := typedData.GetMessageHash(dexAccountAddressBN, message, caigo.StarkCurve{})
-	r, s, _ := caigo.Curve.Sign(messageHash, dexPrivateKeyBN)
+	r, s, _ := GnarkSign(messageHash, dexPrivateKey)
 
 	// URL
 	authUrl := fmt.Sprintf("%s/auth", PARADEX_HTTP_URL)
@@ -162,6 +192,38 @@ func GetOpenOrders(jwtToken string) []*Order {
 
 	orders := ParseGetOrders(res)
 	return orders
+}
+
+func ExampleSignOrder() {
+	privateKey := GetEcdsaPrivateKey("X")
+	accountAddress := big.NewInt(0)
+	sc := caigo.StarkCurve{}
+	td, _ := NewVerificationTypedData("Order", "PRIVATE_SN_POTC_SEPOLIA")
+	domEnc, _ := td.GetTypedMessageHash("StarkNetDomain", td.Domain, sc)
+
+	for j := 0; j < 10; j++ {
+		start := time.Now()
+		for i := 0; i < 100000; i++ {
+			orderP := &OrderPayload{
+				Timestamp: time.Now().Unix(),
+				Market:    "BTC-USD-PERP",
+				Side:      "BUY",
+				OrderType: "LIMIT",
+				Size:      strconv.Itoa(20 + i),
+				Price:     strconv.Itoa(1900 + i),
+			}
+			messageHash, _ := GnarkGetMessageHash(td, domEnc, accountAddress, orderP, sc)
+			sigBin, err := privateKey.Sign(messageHash.Bytes(), nil)
+			if err != nil {
+				fmt.Println(err)
+			}
+			valid, _ := privateKey.PublicKey.Verify(sigBin, messageHash.Bytes(), nil)
+			if !valid {
+				fmt.Println("Invalid signature")
+			}
+		}
+		fmt.Println(time.Since(start).Seconds() / 100000)
+	}
 }
 
 func main() {
