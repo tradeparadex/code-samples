3544b2afcd7ece21dda3f23ffd6ee068825f6230
Tricia Tan <tricia.tzy@tutanota.com>
2024-10-21 16:44:10 +0800

    [go] add grindkey utility function

--- go/utils.go
+++ go/utils.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io"
 	"math/big"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/dontpanicdao/caigo"
 	"github.com/dontpanicdao/caigo/types"
@@ -66,4 +69,48 @@ func ComputeAddress(config SystemConfigResponse, publicKey string) string {
 	}
 	addressHash, _ := caigo.Curve.ComputeHashOnElements(address)
 	return types.BigToHex(addressHash)
+}
+
+func GrindKey(keySeed string, keyValLimit *big.Int) string {
+	sha256EcMaxDigest := new(big.Int)
+	sha256EcMaxDigest.SetString("1 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000", 16)
+	maxAllowedVal := new(big.Int).Sub(sha256EcMaxDigest, new(big.Int).Mod(sha256EcMaxDigest, keyValLimit))
+
+	i := 0
+	key := hashKeyWithIndex(keySeed, i)
+	i++
+
+	// Make sure the produced key is divided by the Stark EC order, and falls within the range
+	// [0, maxAllowedVal).
+	for key.Cmp(maxAllowedVal) < 0 {
+		key = hashKeyWithIndex(keySeed, i)
+		i++
+	}
+
+	// Should this be unsignedMod?
+	result := new(big.Int).Mod(key, keyValLimit)
+	return fmt.Sprintf("0x%x", result)
+}
+
+func hashKeyWithIndex(keySeed string, index int) *big.Int {
+	// Remove '0x' prefix if present
+	key := strings.TrimPrefix(keySeed, "0x")
+
+	// Convert index to hex and pad to 2 bytes
+	indexHex := fmt.Sprintf("%02x", index)
+
+	// Combine key and index
+	data := key + indexHex
+
+	// Decode hex string to bytes
+	dataBytes, err := hex.DecodeString(data)
+	if err != nil {
+		panic(err)
+	}
+
+	// Compute SHA-256 hash
+	hash := sha256.Sum256(dataBytes)
+
+	// Convert hash to big.Int
+	return new(big.Int).SetBytes(hash[:])
 }
