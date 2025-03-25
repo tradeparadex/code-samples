599db47645355c4aa0c449b11e6efd08eb6ab1d3
Adam WEAVER <43756933+aweaver89@users.noreply.github.com>
2025-03-25 15:05:39 +0100

    Apply alt method
    Co-authored-by: Tricia Tan <5356506+trishtzy@users.noreply.github.com>

--- go/example.go
+++ go/example.go
@@ -53,13 +53,7 @@ func GetEthereumAccount() (string, string) {
 // Generate Paradex private key from Ethereum private key
 func GenerateParadexAccount(config SystemConfigResponse, ethPrivateKey string) (string, string, string) {
 	privateKey, _ := crypto.HexToECDSA(ethPrivateKey)
-	l1Chain, _ := strconv.ParseInt(config.L1ChainId, 10, 64)
-
-	// Update the ChainId value in the typedData using config
-	typedData.Domain.ChainId = math.NewHexOrDecimal256(l1Chain)
-
-	ethSignature, _ := SignTypedData(typedData, privateKey)
-
+   ethSignature, _ := SignTypedData(typedData(config.L1ChainId), privateKey)
 	// Convert the first 32 bytes of ethSignature to a hex string
 	r := hex.EncodeToString(ethSignature[:32])
 	// Get Starknet curve order
